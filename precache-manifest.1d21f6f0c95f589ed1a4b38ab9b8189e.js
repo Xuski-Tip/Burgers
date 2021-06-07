2403a8b38280a24fcc0494a971b80d02941edb55
Xuski-Tip <75438787+Xuski-Tip@users.noreply.github.com>
2021-06-07 19:19:16 +0500

    Add files via upload

---
+++ precache-manifest.1d21f6f0c95f589ed1a4b38ab9b8189e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e558a6a68e087225be48c3d66a505336",
+    "url": "/index.html"
+  },
+  {
+    "revision": "752cfc8faab815b142c1",
+    "url": "/static/css/main.a1d94c57.chunk.css"
+  },
+  {
+    "revision": "b8cc6a79d9025239f479",
+    "url": "/static/js/2.739fbba5.chunk.js"
+  },
+  {
+    "revision": "752cfc8faab815b142c1",
+    "url": "/static/js/main.07b9f9a3.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
